f480250a60e6e8978278a497ebe80460accbeddc
Nicolas Donna <donnanicolas@gmail.com>
2015-10-05 18:03:25 -0300

    Using header authorisation instead of query

--- api/middleware/api_key_middleware.py
+++ api/middleware/api_key_middleware.py
@@ -7,11 +7,16 @@ class ApiKeyMiddleware(object):
         if not request.get_full_path().startswith('/api'):
             return None
 
-        if  'key' not in request.REQUEST:
-            return JsonResponse({'result': 'error', 'msg': 'No API key'}, status=400)
+        if  'HTTP_AUTHORIZATION' not in request.META:
+            return JsonResponse({'result': 'error', 'msg': 'Bad Auth'}, status=401)
+
+        auth = request.META['HTTP_AUTHORIZATION']
+        auth = auth.split(' ')
+        if 2 != len(auth):
+            return JsonResponse({'result': 'error', 'msg': 'Bad Auth'}, status=401)
 
         try:
-            key = Key.objects.filter(key = request.REQUEST.get('key')).get()
+            key = Key.objects.filter(key = auth[1]).get()
         except ObjectDoesNotExist as e:
-            return JsonResponse({'result': 'error', 'msg': 'Bad API key'}, status=400)
+            return JsonResponse({'result': 'error', 'msg': 'Bad Auth'}, status=401)
         return None
