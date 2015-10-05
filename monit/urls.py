8b0c53155197c73e6a549818edc00f22487ae2e7
Nicolas Donna <donnanicolas@gmail.com>
2015-10-05 18:13:12 -0300

    Redirect to admin from /

--- monit/urls.py
+++ monit/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import include, url
+from django.views.generic.base import RedirectView
 from django.contrib import admin
 
 urlpatterns = [
@@ -7,5 +8,6 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
     url(r'^api/', include('api.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^.*$', RedirectView.as_view(url='/admin/', permanent=True), name='index'),
 
 ]
